356c1ade83eb665d510dd0d93ac06382b43a748f
RonYakov <r.yakov154@gmail.com>
2024-08-27 18:14:07 +0300

    events and statistics work

--- backend/src/events/events.controller.ts
+++ backend/src/events/events.controller.ts
@@ -1,5 +1,5 @@
 import {Body, Controller, Logger, Post, Get} from '@nestjs/common';
-import { EventsService } from './events.service';
+import { EventsService , CreationFailedError} from './events.service';
 import {CreateEventDto} from "../shared/DTO/create-event.dto";
 import {EditEventDto} from "../shared/DTO/edit-event.dto";
 
@@ -11,15 +11,57 @@ export class EventsController {
   async createAbsoluteEvent(
       @Body() eventData: CreateEventDto
   ) {
-    return await this.eventService.createEvent(eventData);
+    try{
+      await this.eventService.createEvent(eventData);
+      return {status: 200};
+
+    } catch (e) {
+      if (e instanceof CreationFailedError) {
+        return {status: 4001, object1: e.object1};
+      }
+    }
   }
 
   @Post('/edit-absolute-event')
   async editAbsoluteEvent(
       @Body() eventData: EditEventDto
   ) {
-        return await this.eventService.editEvent(eventData);
+    try {
+      await this.eventService.editEvent(eventData);
+      return {status: 200};
+
+    } catch (e) {
+      if (e instanceof CreationFailedError) {
+        return {status: 4001, object1: e.object1};
+      }
     }
+  }
+
+  @Post('/edit-change-events')
+  async editChangeEvents(
+      @Body() eventData: EditEventDto, @Body() changeEvents: EditEventDto[]
+  ) {
+    try {
+      await this.eventService.changeFlexibleEventsToAbsoluteEvents(changeEvents);
+      return {status: 200};
+
+    } catch (e) {
+        return {status: 4001, object1: e.object1};
+    }
+  }
+
+  @Post('/create-change-events')
+  async createChangeEvents(
+      @Body() eventData: CreateEventDto, @Body() changeEvents: EditEventDto[]
+  ) {
+    try{
+      await this.eventService.changeFlexibleEventsToAbsoluteEvents(changeEvents);
+      return {status: 200};
+
+    } catch (e) {
+        return {status: 4001, object1: e.object1};
+    }
+  }
 
 
   @Post('place-event')
@@ -29,5 +71,4 @@ export class EventsController {
     console.log('result: ', result);
     return result;
   }
-
 }
--- backend/src/statistics/statistics.service.ts
+++ backend/src/statistics/statistics.service.ts
@@ -1,8 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import { FlexibleEventEntityService } from '../shared/services/data-base-services/flexible-event-entity/flexible-event-entity.service';
+import { AbsoluteEventEntityService } from '../shared/services/data-base-services/absoulte-event-entity/absolute-event-entity.service';
+import {AbsoluteEvent} from "../shared/entities/absolute-event.entity";
+import {FlexibleEvent} from "../shared/entities/flexible-event.entity";
+import {EventCategoryEnum} from "../shared/enum/event-category.enum";
 
 @Injectable()
 export class StatisticsService {
-    getCurrentMonthEvents(userId: string) {
-        //todo
+    constructor(private absoluteEventEntityService: AbsoluteEventEntityService,
+                private flexibleEventEntityService: FlexibleEventEntityService) {}
+
+    async getCurrentMonthEvents(userId: number) {
+        const now = new Date();
+        const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+        const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+
+        return this.getEventsByDateRange(userId, startOfMonth, endOfMonth);
+    }
+
+    async getCurrentWeekEvents(userId: number) {
+        const now = new Date();
+        const startOfWeek = new Date(now);
+        startOfWeek.setDate(now.getDate() - now.getDay());
+        startOfWeek.setHours(0, 0, 0, 0);
+
+        const endOfWeek = new Date(startOfWeek);
+        endOfWeek.setDate(startOfWeek.getDate() + 6);
+        endOfWeek.setHours(23, 59, 59, 999);
+
+        return this.getEventsByDateRange(userId, startOfWeek, endOfWeek);
+    }
+
+    private async getEventsByDateRange(userId: number, startDate: Date, endDate: Date) {
+        const absoluteEventsPromise = this.absoluteEventEntityService.getEventsByDateRange(userId, startDate, endDate);
+        const flexibleEventsPromise = this.flexibleEventEntityService.getFlexibleEventsByDateRange(userId, startDate, endDate);
+
+        return await this.countEventsByCategory(absoluteEventsPromise, flexibleEventsPromise);
+
+
+    }
+
+    private async countEventsByCategory(
+        absoluteEventsPromise: Promise<AbsoluteEvent[]>,
+        flexibleEventsPromise: Promise<FlexibleEvent[]>
+    ): Promise<Record<string, number>> {
+        const categoryCountObject: Record<string, number> = {};
+
+        Object.values(EventCategoryEnum).forEach(category => {
+            if (typeof category === 'number') {
+                categoryCountObject[EventCategoryEnum[category]] = 0;
+            }
+        });
+
+        const [absoluteEvents, flexibleEvents] = await Promise.all([absoluteEventsPromise, flexibleEventsPromise]);
+
+        absoluteEvents.forEach(event => {
+            const categoryName = EventCategoryEnum[event.category];
+            categoryCountObject[categoryName]++;
+        });
+
+        flexibleEvents.forEach(event => {
+            const categoryName = EventCategoryEnum[event.category];
+            categoryCountObject[categoryName]++;
+        });
+
+        return categoryCountObject;
     }
 }
